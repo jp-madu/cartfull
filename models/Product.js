3a33ed986ec9ea8d71193b7761c5635b0b25f85f
madujp <madu.j.paul@gmail.com>
2024-12-28 19:21:31 -0800

    init commit

---
+++ models/Product.js
@@ -0,0 +1,11 @@
+// Product.js
+class Product {
+  constructor(id, name, price, image) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.image = image;
+  }
+}
+
+export default Product; // Export the class for use in other files
